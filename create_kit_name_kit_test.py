6fd8b46a2b0b7e0ff1fe9c312f40c42badb75082
danielaparedes <dparedese@gmail.com>
2025-01-12 18:43:18 +0100

    primer version

---
+++ create_kit_name_kit_test.py
@@ -0,0 +1,62 @@
+import sender_stand_request
+import data
+
+def get_kit_body(name):
+    current_body = data.kit_body.copy()
+    current_body["name"] = name
+    return current_body
+
+def positive_assert(name):
+    kit_body = get_kit_body(name)
+    kit_response = sender_stand_request.post_new_client_kit(kit_body)
+    assert kit_response.status_code == 201
+
+def negative_assert(name):
+    kit_body = get_kit_body(name)
+    response = sender_stand_request.post_new_client_kit(kit_body)
+    assert response.status_code == 400
+    assert response.json()["code"] == 400
+    assert response.json()["message"] == "El nombre que ingresaste es incorrecto. " \
+                                         "Los nombres deben tener al menos 1 caracter y no más de 512 caracteres"
+def negative_assert_no_name(kit_body):
+    response = sender_stand_request.post_new_client_kit(kit_body)
+    assert response.status_code == 400
+
+#Prueba 1 Nombre con una letra
+def test_create_kit_1_letter_in_name_get_success_response():
+    positive_assert("a")
+
+#Prueba 2 Nombre con 511 letras
+def test_create_kit_511_letters_in_name_get_success_response():
+    positive_assert("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC")
+
+#Prueba 3 Nombre vacío
+def test_create_kit_0_letters_in_name_get_error_response():
+    negative_assert("")
+
+#Prueba 4 Nombre con 512 letras
+def test_create_kit_512_letters_in_name_get_error_response():
+    negative_assert("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD")
+
+#Prueba 5 Nombre con caracteres especiales
+def test_create_kit_special_letters_in_name_get_success_response():
+    positive_assert("№%@")
+
+#Prueba 6 Nombre con espacios
+def test_create_kit_spaces_in_name_get_success_response():
+    positive_assert(" A Aaa ")
+
+#Prueba 7 Nombre con números
+def test_create_kit_numbers_in_name_get_success_response():
+    positive_assert("123")
+
+#Prueba 8 Nombre vacío con cuerpo ya definido
+def test_create_kit_empty_name_get_error_response():
+    kit_body = get_kit_body("")
+    negative_assert_no_name(kit_body)
+
+#Prueba 9 Nombre numérico
+def test_create_kit_number_type_name_get_error_response():
+    kit_body = get_kit_body(123)
+    response = sender_stand_request.post_new_client_kit(kit_body)
+    assert response.status_code == 400
---
+++ data.py
@@ -0,0 +1,20 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Daniela",
+    "phone": "+11234567890",
+    "address": "Municipio Molinicos, Albacete"
+}
+
+kit_body = {
+       "name": "Mi conjunto",
+       "card": {
+           "id": 1,
+           "name": "Para la situación"
+       },
+       "productsList": None,
+       "id": 7,
+       "productsCount": 0
+   }

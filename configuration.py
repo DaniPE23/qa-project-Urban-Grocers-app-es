9fbedc796a2434f92b33547f881163f6328b3311
danielaparedes <dparedese@gmail.com>
2025-01-13 18:18:29 +0100

    correcciones hechas

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-3d8eedcf-effc-438b-9d9d-d5a58c9bc6b1.containerhub.tripleten-services.com"  # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = "https://cnt-6a023fdd-affc-4759-a3ff-13df75174d2c.containerhub.tripleten-services.com"  # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH = "/api/v1/users/" # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = "/api/v1/kits/" # Almacena la ruta para crear un kit en esta variable
--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -24,11 +24,19 @@ def negative_assert_no_name(kit_body):
 
 #Prueba 1 Nombre con una letra
 def test_create_kit_1_letter_in_name_get_success_response():
+    auth_token = "<tu_token_de_acceso>"
     positive_assert("a")
+    response = sender_stand_request.post_new_client_kit(kit_body, auth_token)
+    assert response.status_code == 201
+    assert response.json().get("name") == "a"
 
 #Prueba 2 Nombre con 511 letras
 def test_create_kit_511_letters_in_name_get_success_response():
+    auth_token = "<tu_token_de_acceso>"
     positive_assert("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC")
+    response = sender_stand_request.post_new_client_kit(kit_body, auth_token)
+    assert response.status_code == 201
+    assert response.json().get("name") == "AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC"
 
 #Prueba 3 Nombre vacío
 def test_create_kit_0_letters_in_name_get_error_response():
@@ -40,15 +48,27 @@ def test_create_kit_512_letters_in_name_get_error_response():
 
 #Prueba 5 Nombre con caracteres especiales
 def test_create_kit_special_letters_in_name_get_success_response():
+    auth_token = "<tu_token_de_acceso>"
     positive_assert("№%@")
+    response = sender_stand_request.post_new_client_kit(kit_body, auth_token)
+    assert response.status_code == 201
+    assert response.json().get("name") == "№%@"
 
 #Prueba 6 Nombre con espacios
 def test_create_kit_spaces_in_name_get_success_response():
-    positive_assert(" A Aaa ")
+    auth_token = "<tu_token_de_acceso>"
+    positive_assert(" A Aaa" )
+    response = sender_stand_request.post_new_client_kit(kit_body, auth_token)
+    assert response.status_code == 201
+    assert response.json().get("name") == " A Aaa"
 
 #Prueba 7 Nombre con números
 def test_create_kit_numbers_in_name_get_success_response():
+    auth_token = "<tu_token_de_acceso>"
     positive_assert("123")
+    response = sender_stand_request.post_new_client_kit(kit_body, auth_token)
+    assert response.status_code == 201
+    assert response.json().get("name") == "123"
 
 #Prueba 8 Nombre vacío con cuerpo ya definido
 def test_create_kit_empty_name_get_error_response():
